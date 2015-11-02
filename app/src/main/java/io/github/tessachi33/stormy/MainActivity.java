0e664ab2896eb8ba59601c152b3f69270a3e2120
Tessa <Tessachi33@yahoo.com>
2015-11-02 12:04:57 -0800

    Add incorrect synchronous Get to see the wrong was, correction to follow

--- app/src/main/java/io/github/tessachi33/stormy/MainActivity.java
+++ app/src/main/java/io/github/tessachi33/stormy/MainActivity.java
@@ -1,17 +1,21 @@
 package io.github.tessachi33.stormy;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -19,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
         double latitude = 37.8267;
         double longitude= -122.423;
         String forecastUrl = "https://api.forecast.io/forecast/"+ apiKey +"/" + latitude + "," +longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastUrl).build();
+
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "exception caught  ", e);
+        }
+
     }
 }
