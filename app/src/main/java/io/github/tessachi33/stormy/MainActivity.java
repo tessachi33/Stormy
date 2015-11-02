052d57337c4f0805e3ccdd5371aca83ce734776e
Tessa <Tessachi33@yahoo.com>
2015-11-02 12:22:20 -0800

    WIP Asynchronous Get, import callback

--- app/src/main/java/io/github/tessachi33/stormy/MainActivity.java
+++ app/src/main/java/io/github/tessachi33/stormy/MainActivity.java
@@ -3,9 +3,8 @@ package io.github.tessachi33.stormy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
-
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -22,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         String apiKey = "f5cc6334f7b3eb4e50bc2b418375041f";
         double latitude = 37.8267;
         double longitude= -122.423;
-        String forecastUrl = "https://api.forecast.io/forecast/"+ apiKey +"/" + latitude + "," +longitude;
+        String forecastUrl = "https://api.forecast.io/forecast/"+ apiKey +"/" + latitude + "," + longitude;
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(forecastUrl).build();
