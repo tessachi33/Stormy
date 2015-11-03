21f1b64d4afc56b5a83ebbde3349f6a5fa5dcb0d
Tessa <Tessachi33@yahoo.com>
2015-11-03 09:39:46 -0800

    Add getters and setters for all

--- app/src/main/java/io/github/tessachi33/stormy/CurrentWeather.java
+++ app/src/main/java/io/github/tessachi33/stormy/CurrentWeather.java
@@ -11,4 +11,54 @@ public class CurrentWeather {
     private double mHumidity;
     private double mPercipChance;
 
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPercipChance() {
+        return mPercipChance;
+    }
+
+    public void setPercipChance(double percipChance) {
+        mPercipChance = percipChance;
+    }
+
+    public String getMsummary() {
+        return msummary;
+    }
+
+    public void setMsummary(String msummary) {
+        this.msummary = msummary;
+    }
+
+    private String msummary;
+
 }
