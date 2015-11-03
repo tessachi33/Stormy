773a2bcc65bb77668453909d0393a898c335b109
Tessa <Tessachi33@yahoo.com>
2015-11-03 09:50:55 -0800

    Add mCurrentWeather to MainActvity

--- app/src/main/java/io/github/tessachi33/stormy/MainActivity.java
+++ app/src/main/java/io/github/tessachi33/stormy/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private CurrentWeather mCurrentWeather;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
