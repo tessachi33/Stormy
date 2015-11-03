95cc83fd20a1876a46a8d6c070fff8357c675d32
Tessa <Tessachi33@yahoo.com>
2015-11-03 10:16:01 -0800

    Add AlertdialogFragment

--- app/src/main/java/AlertDialogFragment.java
+++ app/src/main/java/AlertDialogFragment.java
@@ -1,5 +1,25 @@
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+import io.github.tessachi33.stormy.R;
+
 /**
  * Created by Tessa on 11/3/15.
  */
-public class AlertDialogFragment {
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_ok_button_text, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
 }
