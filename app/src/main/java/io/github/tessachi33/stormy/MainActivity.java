eea2c23d00cd71af9a82367082e893a9e2e6f499
Tessa <Tessachi33@yahoo.com>
2015-11-03 14:54:28 -0800

    Add all @Bind

--- app/src/main/java/io/github/tessachi33/stormy/MainActivity.java
+++ app/src/main/java/io/github/tessachi33/stormy/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
@@ -15,15 +18,26 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private CurrentWeather mCurrentWeather;
+    @Bind(R.id.timeLabel) TextView mTimeLabel;
+    @Bind(R.id.tempuratureLabel) TextView mTempatureLabel;
+    @Bind(R.id.humidityValue) TextView mHumidityValue;
+    @Bind(R.id.precipValue) TextView mPrecipValue;
+    @Bind(R.id.iconImageView) ImageView mIconImageView;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         String apiKey = "f5cc6334f7b3eb4e50bc2b418375041f";
         double latitude = 32.7833;
         double longitude= 79.9333;
